ff52452e8e5563e5963861e58fe14540b1d862db
Johannes Riedmann <ae9614@student.jamk.fi>
2024-04-07 22:37:59 +0200

    Connect to MQTT broker from TransflowAnalyzer

--- TransflowAnalyzer/Program.cs
+++ TransflowAnalyzer/Program.cs
@@ -1,14 +1,19 @@
+using MQTTnet.Client;
 using TransflowAnalyzer.Api.Services;
+using TransflowAnalyzer.Sources.Messaging;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var mqttClient = await MqttServiceFactory.CreateAsync("localhost", "transflow_analyzer");
+
 // Add services to the container.
 builder.Services.AddGrpc();
+builder.Services.AddSingleton(mqttClient);
 
-var app = builder.Build();
+var webApi = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapGet("/", () => "This service only supports communication through a gRPC client. HTTP/1.1 GET request is not supported.");
-app.MapGrpcService<ExampleService>();
+webApi.MapGet("/", () => "This service only supports communication through a gRPC client. HTTP/1.1 GET request is not supported.");
+webApi.MapGrpcService<ExampleService>();
 
-app.Run();
+webApi.Run();
---
+++ TransflowAnalyzer/Sources/Messaging/MqttServiceFactory.cs
@@ -0,0 +1,35 @@
+﻿using MQTTnet.Client;
+using MQTTnet;
+
+namespace TransflowAnalyzer.Sources.Messaging
+{
+    public static class MqttServiceFactory
+    {
+        public static async Task<IMqttClient> CreateAsync(string brokerUrl, string clientId)
+        {
+            try
+            {
+                var options = new MqttClientOptionsBuilder()
+                    .WithTcpServer(brokerUrl)
+                    .WithClientId(clientId)
+                    .WithCleanSession()
+                    .Build();
+
+                var mqttClient = new MqttFactory().CreateMqttClient();
+                var connectionResult = await mqttClient.ConnectAsync(options, CancellationToken.None);
+
+                if (connectionResult.ResultCode != MqttClientConnectResultCode.Success)
+                {
+                    throw new IOException($"Failed to establish connection to MQTT broker at URL: {brokerUrl};\n" +
+                        $"Status code: {connectionResult.ResultCode}");
+                }
+
+                return mqttClient;
+            }
+            catch (Exception exp)
+            {
+                throw new IOException("Ran into error when connecting to MQTT broker! Reason: " + exp.Message);
+            }
+        }
+    }
+}
