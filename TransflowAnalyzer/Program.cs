3138f318f904416e61bf085d252db5950cd68c30
Johannes Riedmann <ae9614@student.jamk.fi>
2024-06-06 19:45:23 +0200

    Read environment variables from .env file in Transflow Analyzer

--- TransflowAnalyzer/Program.cs
+++ TransflowAnalyzer/Program.cs
@@ -1,19 +1,41 @@
-using MQTTnet.Client;
 using TransflowAnalyzer.Api.Services;
 using TransflowAnalyzer.Sources.Messaging;
+using DotNetEnv;
 
-var builder = WebApplication.CreateBuilder(args);
 
-var mqttClient = await MqttServiceFactory.CreateAsync("localhost", "transflow_analyzer");
+try
+{
+    // load environment variables from the .env file immediately outside this subproject's root
+    Env.Load("../runtime.env");
 
-// Add services to the container.
-builder.Services.AddGrpc();
-builder.Services.AddSingleton(mqttClient);
+    var builder = WebApplication.CreateBuilder(args);
 
-var webApi = builder.Build();
 
-// Configure the HTTP request pipeline.
-webApi.MapGet("/", () => "This service only supports communication through a gRPC client. HTTP/1.1 GET request is not supported.");
-webApi.MapGrpcService<ExampleService>();
+    string? metricsTopic = Environment.GetEnvironmentVariable("MQTT_METRICS_TOPIC");
+    string? mqttClientId = Environment.GetEnvironmentVariable("MQTT_ANALYZER_ID");
+    string? mqttMetricsTopic = Environment.GetEnvironmentVariable("MQTT_METRICS_TOPIC");
 
-webApi.Run();
+    if (metricsTopic is null || mqttClientId is null || mqttMetricsTopic is null)
+        throw new Exception("Failed to find required environment variables!");
+
+    var mqttClient = await MqttServiceFactory.CreateAsync("localhost", mqttClientId, [mqttMetricsTopic]);
+
+
+    // Add services to the container.
+    builder.Services.AddGrpc();
+    builder.Services.AddSingleton(mqttClient);
+
+    var webApi = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    webApi.MapGet("/", () => "This service only supports communication through a gRPC client. HTTP/1.1 GET request is not supported.");
+    webApi.MapGrpcService<ExampleService>();
+
+    webApi.Run();
+}
+catch (Exception exp)
+{
+    Console.Error.WriteLine(exp.ToString());
+    Environment.Exit(100);
+}
+
--- TransflowAnalyzer/Sources/Messaging/MqttServiceFactory.cs
+++ TransflowAnalyzer/Sources/Messaging/MqttServiceFactory.cs
@@ -1,15 +1,17 @@
 ﻿using MQTTnet.Client;
 using MQTTnet;
+using MQTTnet.Protocol;
 
 namespace TransflowAnalyzer.Sources.Messaging
 {
     public static class MqttServiceFactory
     {
-        public static async Task<IMqttClient> CreateAsync(string brokerUrl, string clientId)
+        public static async Task<IMqttClient> CreateAsync(string brokerUrl, string clientId, string[] topics)
         {
             try
             {
                 var options = new MqttClientOptionsBuilder()
+                    .WithProtocolVersion(MQTTnet.Formatter.MqttProtocolVersion.V500)
                     .WithTcpServer(brokerUrl)
                     .WithClientId(clientId)
                     .WithCleanSession()
@@ -23,6 +25,13 @@ namespace TransflowAnalyzer.Sources.Messaging
                     throw new IOException($"Failed to establish connection to MQTT broker at URL: {brokerUrl};\n" +
                         $"Status code: {connectionResult.ResultCode}");
                 }
+
+                // subscribe to every topic
+                foreach (var topic in topics)
+                {
+                    await mqttClient.SubscribeAsync(topic, MqttQualityOfServiceLevel.ExactlyOnce);
+                }
+
 
                 return mqttClient;
             }
