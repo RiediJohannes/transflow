ec8e416de8920c19d6a65c683f0273f5c8c94a44
Johannes Riedmann <ae9614@student.jamk.fi>
2024-06-08 17:32:49 +0200

    Connected WASM web client to gRPC API

--- TransflowConsumer/TransflowAnalyzer/Program.cs
+++ TransflowConsumer/TransflowAnalyzer/Program.cs
@@ -1,4 +1,6 @@
 using DotNetEnv;
+using Google.Protobuf.WellKnownTypes;
+using Microsoft.AspNetCore.Cors.Infrastructure;
 using System.IO.Compression;
 using TransflowAnalyzer.Analysis.Memory;
 using TransflowAnalyzer.Api.Services;
@@ -30,15 +32,46 @@ try
     builder.Services.AddSingleton<SimulationStorage>();
     builder.Services.AddSingleton(mqttParameters);
     builder.Services.AddHostedService<MqttConsumerService>();
+
     builder.Services.AddGrpc(
         config => { config.ResponseCompressionLevel = CompressionLevel.Fastest; }
     );
 
+    builder.Services.AddCors(options =>
+    {
+        options.AddPolicy("AllowAllGrpc", builder =>
+        {
+            builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .WithExposedHeaders("Grpc-Status", "Grpc-Message", "Grpc-Encoding", "Grpc-Accept-Encoding");
+        });
+
+        // stricter alternative: Only allow specific origins (the domains of specific clients)
+        //options.AddPolicy(name: "TransflowVisualizerPolicy",
+        //    policy =>
+        //    {
+        //        policy.WithOrigins("https://localhost:7013", "http://localhost:5159")
+        //            .AllowAnyHeader()
+        //            .AllowAnyMethod();
+        //    });
+    });
+
+
     var webApi = builder.Build();
+
+    if (webApi.Environment.IsDevelopment())
+    {
+        webApi.UseWebAssemblyDebugging();
+    }
+
+    webApi.UseRouting();
+    webApi.UseCors();
 
     // Configure the HTTP request pipeline.
     webApi.MapGet("/", () => "This service only supports communication through a gRPC client. HTTP/1.1 GET request is not supported.");
-    webApi.MapGrpcService<ExampleService>();
+    webApi.UseGrpcWeb();
+    webApi.MapGrpcService<ExampleService>().EnableGrpcWeb().RequireCors("AllowAllGrpc");
 
     webApi.Run();
 }
--- TransflowConsumer/TransflowVisualizer/Program.cs
+++ TransflowConsumer/TransflowVisualizer/Program.cs
@@ -1,11 +1,25 @@
+using Grpc.Net.Client;
+using Grpc.Net.Client.Web;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using TransflowConsumer.Shared.Protobuffs;
 using TransflowVisualizer;
+
+
+const string transflowApiUrl = "https://localhost:7053";
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddSingleton(services =>
+{
+    var httpClient = new HttpClient(new GrpcWebHandler(GrpcWebMode.GrpcWeb, new HttpClientHandler()));
+    var channel = GrpcChannel.ForAddress(transflowApiUrl, new GrpcChannelOptions { HttpClient = httpClient });
+
+    return new Greeter.GreeterClient(channel);
+});
 
 await builder.Build().RunAsync();
