216df7390922e9ed143e801b87dd4d34992b9842
Johannes Riedmann <johannes.riedmann.vlbg@gmail.com>
2024-06-16 20:41:43 +0200

    Visualizer: Display speed and fuel consumption over time in charts

---
+++ TransflowConsumer/TransflowConsumer.Shared/Extensions/CollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace TransflowConsumer.Shared.Extensions
+{
+    public static class CollectionExtensions
+    {
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T>? collection)
+        {
+            return collection is null || !collection.Any();
+        }
+
+        public static bool HasItems<T>(this IEnumerable<T>? collection)
+        {
+            return !collection.IsNullOrEmpty();
+        }
+    }
+}
